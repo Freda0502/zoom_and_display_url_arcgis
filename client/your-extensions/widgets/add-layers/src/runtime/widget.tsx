ebd6e2d5c58ac9646d96d45fbd0db2e96f128a02
Freda0502 <68989706+Freda0502@users.noreply.github.com>
2021-08-04 22:34:22 -0500

    cleaned up the code. Removed console logs

--- client/your-extensions/widgets/add-layers/src/runtime/widget.tsx
+++ client/your-extensions/widgets/add-layers/src/runtime/widget.tsx
@@ -1,4 +1,4 @@
-import { React, styled, AllWidgetProps } from 'jimu-core'
+import { React, styled, AllWidgetProps, Layer } from 'jimu-core'
 import { IMConfig } from '../config'
 import {
   loadArcGISJSAPIModules,
@@ -7,114 +7,96 @@ import {
 } from 'jimu-arcgis'
 
 import { useState } from 'react'
-import defaultMessages from './translations/default'
 
 export default function Widget(props: AllWidgetProps<IMConfig>) {
   const [jimuMapView, setJimuMapView] = useState<JimuMapView>(null)
-  const [featureServiceUrlInput, setFeatureServiceUrlInput] = useState('')
-  const listToDisplay = props.config.layersList
-  console.log('listToDisplay', listToDisplay)
-  // console.log('listToDisplay', listToDisplay)
 
-  // const [htmlElement, listToDisplay] = Setting(props)
-  // console.log('props,layerslst', props.layersLst) //returns undefined
+  if (props.config.layersList == null) {
+    return <div>no layers to display</div>
+  } else {
+    const listToDisplay = props.config.layersList
+    const zoom = (layerUrl: string) => {
+      if (!jimuMapView) {
+        console.error('Please configure a Data Source with the widget.')
+        return
+      }
 
-  // const handleFeatureServiceUrlInputChange = (event) => {
-  //   setFeatureServiceUrlInput(event.target.value)
-  // }
-
-  //change this: rename, change to always zoom.
-  const zoom = (layerUrl: string) => {
-    if (!jimuMapView) {
-      console.error('Please configure a Data Source with the widget.')
-      return
-    }
-
-    loadArcGISJSAPIModules([
-      'esri/layers/FeatureLayer',
-      'esri/tasks/support/Query',
-      'esri/geometry/SpatialReference',
-    ]).then((modules) => {
-      const [FeatureLayer, Query, SpatialReference] = modules
-      const layer = new FeatureLayer({
-        url: layerUrl,
-      })
-
-      jimuMapView.view.map.add(layer)
-
-      layer.on('layerview-create', (event) => {
-        //let always be true
-        // if (
-        //   props.config.hasOwnProperty('zoomToLayer') &&
-        //   props.config.zoomToLayer === true
-        // )
-        const query: __esri.Query = new Query()
-        query.where = '1=1'
-        query.outSpatialReference = new SpatialReference({
-          wkid: 102100,
+      loadArcGISJSAPIModules([
+        'esri/layers/FeatureLayer',
+        'esri/tasks/support/Query',
+        'esri/geometry/SpatialReference',
+      ]).then((modules) => {
+        const [FeatureLayer, Query, SpatialReference] = modules
+        const layer = new FeatureLayer({
+          url: layerUrl,
         })
-        layer.queryExtent(query).then((results) => {
-          jimuMapView.view.extent = results.extent
+
+        jimuMapView.view.map.add(layer)
+
+        layer.on('layerview-create', (event) => {
+          const query: __esri.Query = new Query()
+          query.where = '1=1'
+          query.outSpatialReference = new SpatialReference({
+            wkid: 102100,
+          })
+          layer.queryExtent(query).then((results) => {
+            jimuMapView.view.extent = results.extent
+          })
         })
       })
-    })
-  }
-
-  const StyleDiv = styled.div`
-    form > div {
-      display: flex;
-      justify-content: space-between;
-      input {
-        width: 100%;
-      }
-      button {
-        min-width: 100px;
-      }
     }
-  `
-  const listItems = listToDisplay.map((obj) => (
-    <button key={obj.id} onClick={() => zoom(obj.layerUrl)}>
-      {/* onclick='zoom({obj.layerUrl.toString())}' */}
-      {obj.name}
-    </button>
-  ))
 
-  return (
-    <StyleDiv>
-      <div className='widget-addLayers jimu-widget p-2'>
-        {props.hasOwnProperty('useMapWidgetIds') &&
-          props.useMapWidgetIds &&
-          props.useMapWidgetIds.length === 1 && (
-            <JimuMapViewComponent
-              useMapWidgetId={props.useMapWidgetIds[0]}
-              onActiveViewChange={(jmv: JimuMapView) => {
-                if (jmv) {
-                  setJimuMapView(jmv)
-                }
-              }}
-            />
-          )}
-        <p>{defaultMessages.instructions}</p>
-        {/* <form onSubmit={zoom}>
-          <div> */}
-        {/* don't need form and input
-            <input
-              type='text'
-              placeholder={defaultMessages.featureServiceUrl}
-              value={featureServiceUrlInput}
-              onChange={handleFeatureServiceUrlInputChange}
-              onClick={() => zoom((layerurl = ''))}
-            /> */}
+    const ButtonDiv = styled.button`
+      font-size: 15px;
+      padding: 12px 29px;
+      width: 170px;
+      text-align: center;
+    `
 
-        {/* button around name of layer. onclick is formSubmit, but rename! one
-            button for each layer in the array, when click on the button,
-            trigger func to add layer to the map */}
-        {/* <button>{defaultMessages.addLayer}</button> */}
-        {/* </div> */}
-        {/* </form> */}
-        <div>URLs to layers in the selected datasource:</div>
-        <p>{listItems}</p>
+    const listItems = listToDisplay.map((obj) => (
+      <div>
+        <ButtonDiv
+          key={obj.id}
+          onClick={() => {
+            zoom(obj.layerUrl)
+          }}
+        >
+          {obj.name}
+        </ButtonDiv>
       </div>
-    </StyleDiv>
-  )
+    ))
+
+    // On an active view change, set the extent State property
+    const onActiveViewChange = (jimuMapView: JimuMapView) => {
+      if (jimuMapView) {
+        setJimuMapView(jimuMapView)
+      }
+    }
+
+    const activeViewChangeHandler = (jmv: JimuMapView) => {
+      if (jmv) {
+        this.setState({
+          jimuMapView: jmv,
+        })
+      }
+    }
+
+    return (
+      <div>
+        <div className='widget-addLayers jimu-widget p-2'>
+          {props.hasOwnProperty('useMapWidgetIds') &&
+            props.useMapWidgetIds &&
+            props.useMapWidgetIds.length === 1 && (
+              <JimuMapViewComponent
+                useMapWidgetId={props.useMapWidgetIds[0]}
+                onActiveViewChange={onActiveViewChange}
+              />
+            )}
+
+          <div>Layers in the selected datasource:</div>
+          <p>{listItems}</p>
+        </div>
+      </div>
+    )
+  }
 }
--- client/your-extensions/widgets/add-layers/src/setting/setting.tsx
+++ client/your-extensions/widgets/add-layers/src/setting/setting.tsx
@@ -1,15 +1,14 @@
 import {
   React,
-  FormattedMessage,
   Immutable,
   UseDataSource,
   DataSourceManager,
   QueriableDataSource,
   styled,
   Layer,
+  FeatureLayerDataSource,
 } from 'jimu-core'
 import { AllWidgetSettingProps } from 'jimu-for-builder'
-import { Switch } from 'jimu-ui'
 import {
   JimuMapViewSelector,
   SettingSection,
@@ -23,30 +22,15 @@ import {
   AllDataSourceTypes,
 } from 'jimu-ui/advanced/data-source-selector'
 import { WebMapDataSourceImpl } from 'jimu-arcgis/lib/data-sources'
-import { useState } from 'react'
-
-// var layersList = []
+import { useState, useEffect } from 'react'
 
 export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
-  // const [layersList, setLayersList] = useState<Array<Layer>>([])
-  //test:
-  // const [layersLst, setLayersLst] = useState<Array<Layer>>([])
-
   const onLayersListAdd = (layers: Layer[]) => {
     props.onSettingChange({
       id: props.id,
       config: props.config.set('layersList', layers),
     })
   }
-
-  // const onZoomToLayerPropertyChange = (
-  //   evt: React.FormEvent<HTMLInputElement>
-  // ) => {
-  //   props.onSettingChange({
-  //     id: props.id,
-  //     config: props.config.set('zoomToLayer', evt.currentTarget.checked),
-  //   })
-  // }
 
   const onMapWidgetSelected = (useMapWidgetIds: string[]) => {
     props.onSettingChange({
@@ -78,46 +62,43 @@ export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
       }
     }
   `
-
-  if (props.useDataSources && props.useDataSources[0]) {
+  function getLayers() {
     const id = props.useDataSources[0].mainDataSourceId
     const dsManager = DataSourceManager.getInstance()
-    // const what = dsManager.getDataSource(id)
-    // what.name
     const mapDataSource = dsManager.getDataSource(id) as WebMapDataSourceImpl
 
     if (mapDataSource && mapDataSource.isDataSourceSet) {
       const dataSourceChildren = mapDataSource.getChildDataSources()
       const layersList = []
-      console.log('dataSourceChildren', dataSourceChildren)
       for (var i = 0; i < dataSourceChildren.length; i++) {
         const childId = dataSourceChildren[i].id
         const startIndex = childId.indexOf('-')
         const idInput = childId.slice(startIndex + 1)
 
-        if (idInput && mapDataSource) {
-          //dup mapDataSource check ??
-          const newUrl = (mapDataSource.getDataSourceByLayer(
+        if (idInput) {
+          const layerUrl = (mapDataSource.getDataSourceByLayer(
             idInput
           ) as QueriableDataSource).url
-          //executed way too many times for some reason
-          console.log(
-            'data source contains what properties?',
-            mapDataSource.getDataSourceByLayer(idInput) as QueriableDataSource
-          )
-          // const newName = (mapDataSource.getDataSourceByLayer(
-          //   idInput
-          // ) as QueriableDataSource).name
-          // const newName = mapDataSource.name
-          // cosnt obj={,,}
-          layersList.push({ idInput, newUrl }) //push an obj instead of url
-          // use setLayersList
+          const name = (mapDataSource.getDataSourceByLayer(
+            idInput
+          ) as FeatureLayerDataSource).getLayerDefinition().name
+          layersList.push({ idInput, layerUrl, name })
         }
       }
-      onLayersListAdd(layersList) //3vals sep by caomma
-      console.log('layersList', layersList)
+      onLayersListAdd(layersList)
+      return layersList
     }
   }
+
+  useEffect(() => {
+    var objLst: Layer[] = []
+    if (props.useDataSources && props.useDataSources[0]) {
+      objLst = getLayers()
+    }
+    return () => {
+      objLst
+    }
+  }, [props.useDataSources])
 
   return (
     <StyleDiv>
@@ -142,24 +123,7 @@ export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
             id: 'settingsLabel',
             defaultMessage: defaultMessages.settings,
           })}
-        >
-          {/* <SettingRow>
-            <div className='w-100 addLayers'>
-              <div className='checkbox-row'>
-                <label>
-                  <FormattedMessage
-                    id='zoomToLayer'
-                    defaultMessage={defaultMessages.zoomToLayer}
-                  />
-                </label>
-                <Switch
-                  checked={(props.config && props.config.zoomToLayer) || false}
-                  // onChange={onZoomToLayerPropertyChange}
-                />
-              </div>
-            </div>
-          </SettingRow> */}
-        </SettingSection>
+        ></SettingSection>
 
         <div className='use-feature-layer-setting p-2'>
           <DataSourceSelector
@@ -175,5 +139,3 @@ export default function Setting(props: AllWidgetSettingProps<IMConfig>) {
     </StyleDiv>
   )
 }
-
-// export { layersList }
